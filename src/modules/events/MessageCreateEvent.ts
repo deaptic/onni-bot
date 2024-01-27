4f1942ef09c12f49d0aa124a3ab74d503186399f
Teo Temonen <teo.temonen@proton.me>
2024-01-27 12:46:14 +0200

    Refactor AI conversation handling in MessageCreateEvent

--- src/modules/events/MessageCreateEvent.ts
+++ src/modules/events/MessageCreateEvent.ts
@@ -2,7 +2,6 @@ import { Events, Message } from "discord.js";
 import { EventBuilder } from "@builders/EventBuilder.ts";
 import { DiscordEvent } from "@modules/events/DiscordEvent.ts";
 import { AI } from "@services/AIService.ts";
-import { Guard } from "@services/GuardService.ts";
 import { Logger } from "@services/LoggerService.ts";
 import { clearTyping, startTyping } from "@utilities/message.ts";
 
@@ -11,47 +10,75 @@ const allowedGuilds = ["614160132712038469", "552089273579470849"];
 export class MessageCreateEvent extends DiscordEvent<Events.MessageCreate> {
   public readonly data = new EventBuilder().setName(Events.MessageCreate);
 
-  private authorization(message: Message) {
-    // Only allow in specific guilds
-    Guard.Against.False(allowedGuilds.includes(message.guild?.id ?? ""));
+  private async handleAIConversation(message: Message) {
+    // Authorization
+    if (!allowedGuilds.includes(message.guild?.id ?? "")) return;
+    if (message.author.bot) return;
+    if (!message.mentions.has(this.client.user?.id ?? "")) return;
 
-    // Only allow if user is not a bot
-    Guard.Against.True(message.author.bot);
-
-    // Only allow if self is available
-    Guard.Against.Null(this.client.user);
-
-    // Only allow if message mentions self
-    Guard.Against.False(message.mentions.has(this.client.user.id));
-
-    return true;
-  }
-
-  public async execute(message: Message) {
-    if (!this.authorization(message)) return;
-
-    const context = {
-      role: "user" as const,
-      content: message.content,
-    };
-
-    startTyping(message);
-
-    const response = await AI.conversation(message.channel.id, context).catch((error) => {
-      Logger.error(`Error while trying to continue conversation`, error);
-    });
-
-    clearTyping(message);
-
-    const chunks = response?.match(/(.|[\r\n]){1,2000}/g);
-
-    for (const chunk of chunks ?? []) {
+    // AI is not available between 12am and 10am
+    const hour = new Date().getHours();
+    if (hour >= 0 && hour < 10) {
       await message.reply({
-        content: chunk,
+        content: "I'm sleeping right now. I'll be back at 10am.",
         allowedMentions: {
           repliedUser: true,
         },
       });
+      return;
     }
+
+    // Get or create thread
+    const thread = message.channel.isThread() ? message.channel : await message.startThread({
+      name: message.cleanContent,
+      autoArchiveDuration: 60,
+    });
+
+    // Start typing indicator
+    startTyping(message);
+
+    // Start conversation
+    const response = await AI.conversation(thread.id, {
+      role: "user" as const,
+      content: message.content,
+    }).catch((error) => {
+      Logger.error(`Error while trying to continue conversation`, error);
+    });
+
+    // Stop typing indicator
+    clearTyping(message);
+
+    // No response from AI
+    if (!response) {
+      await message.reply({
+        content: "I'm sorry, I couldn't understand that.",
+        allowedMentions: {
+          repliedUser: true,
+        },
+      });
+      return;
+    }
+
+    // AI response
+    const chunks = response?.match(/(.|[\r\n]){1,2000}/g);
+
+    for (const chunk of chunks ?? []) {
+      if (message.channel.isThread()) {
+        await message.reply({
+          content: chunk,
+          allowedMentions: {
+            repliedUser: true,
+          },
+        });
+        continue;
+      }
+      await thread.send({
+        content: chunk,
+      });
+    }
+  }
+
+  public async execute(message: Message) {
+    await this.handleAIConversation(message);
   }
 }
